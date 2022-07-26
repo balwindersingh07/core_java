1295fa2a1f1e49a7c3b5438022405fe3784c5a01
Balwinder Singh <77114968+balwindersingh07@users.noreply.github.com>
2022-07-26 13:24:37 +0530

    Create Test.java
    test uploaded

---
+++ Test.java
@@ -0,0 +1,83 @@
+//QUESTION NO.1
+
+class Test{
+	
+	static String name = "Balwinder Singh";
+	static	int age = 20;	
+	
+	public static void main(String []args){
+			hobby();
+	}
+	public static void hobby(){
+		System.out.println("My name is: " + name);
+		System.out.println("And my age is: " + age);
+		
+	}
+	
+	
+}
+
+//QUESTION NO.2
+
+/* EXAMPLES OF NAMES : 
+    
+	1.CLASS : MyClass,
+	          ThisIsMyClass
+    
+	2.FUNCTION : myFunction();
+                 printHello();
+    
+	3.VARIABLE : myVar,
+	             newVariable
+				 
+	4.STATIC VARIABLE : MY_VARIABLE,
+	                    HELLO_VAR,
+
+
+*/
+
+
+
+//QUESTION N0.3
+
+/* 
+   ANS 1: .java is the extension used for creating the java files in the computer
+   systems. Java programs that we write in java language is always saved as filename.java
+   Then we use java compiler to compile the saved file and the copiler takes the file
+   with .java extension and after that return us with the .class extension file.
+   So, .class is file that is created after the compiliation which is executeable file
+   then we can esily run that file in our system and it shows the output.
+   
+   
+   
+   ANS 2: No Java is not a complete OOP's programming language because there are primitive 
+   data types in it which are not same as objects so we have to use wrapper classes for it 
+   thats why java is not completely OOP's programming language.
+  
+   
+   
+   
+   ANS 3: Example of (NR-WA)
+        
+		public void sum(int a, int b){
+			int c = a + b;
+			
+		} 
+		
+		This function have return type void which means no return value and it takes two 
+		arguments that are a,b.
+   
+
+
+*/
+
+//QUESTION NO.5 
+
+/*
+  Wraper Classes: These are used to converts the primitive data types into the objects.
+  For example, we have int a, primitive
+             and if we create new
+			 int a;
+			 Integer i2 = a;
+ so in this way we can use it to convert it.
+*/
